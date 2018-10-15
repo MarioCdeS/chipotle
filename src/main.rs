9f051385eb6d173892f24c7479d62abdfb0f379e
Mario de Sousa <desousa.mc@gmail.com>
2018-10-15 11:41:26 +0200

    EoS 2018-10-15 11:40

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ extern crate chipotle;
 
 use stdweb::traits::*;
 use stdweb::unstable::TryInto;
-use stdweb::web::{self, FileList};
-use stdweb::web::event::ChangeEvent;
+use stdweb::web::{self, FileList, FileReader, FileReaderResult};
+use stdweb::web::event::{ChangeEvent, ProgressLoadEvent};
 use stdweb::web::html_element::{CanvasElement, InputElement};
 
 use chipotle::Emulator;
@@ -25,27 +25,41 @@ fn main() {
         .get_element_by_id("load-rom-button")
         .unwrap();
 
-    load_button.add_event_listener(move |event: ChangeEvent| {
-        let input: InputElement = event.target()
-            .unwrap()
-            .try_into()
-            .unwrap();
+    load_button.add_event_listener(load_button_event_listener);
 
-        let files: FileList = js! {
-            return @{input}.files;
-        }.try_into().unwrap();
-
-        let file = match files.iter().next() {
-            Some(file) => file,
-            None => return,
-        };
-
-        js! {
-            console.log(@{file});
-        }
-    });
-    
     Emulator::start(&emulator);
 
     stdweb::event_loop();
+}
+
+fn load_button_event_listener(event: ChangeEvent) {
+    let input: InputElement = event.target()
+        .unwrap()
+        .try_into()
+        .unwrap();
+
+    let files: FileList = js! {
+        return @{input}.files;
+    }.try_into().unwrap();
+
+    let file = match files.iter().next() {
+        Some(file) => file,
+        None => return,
+    };
+
+    let reader = FileReader::new();
+    reader.add_event_listener({
+        let reader = reader.clone();
+        move |_: ProgressLoadEvent| {
+            let text = match reader.result() {
+                Some(FileReaderResult::String(text)) => text,
+                _ => String::from(""),
+            };
+
+            js! {
+                console.log(@{text});
+            }
+        }
+    });
+    reader.read_as_text(&file).unwrap();
 }
